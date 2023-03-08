8224ffec82b76930062c5248ff1b963bce5920cd
trielt1 <14023937+trielt1@users.noreply.github.com>
2023-03-08 14:35:52 -0300

    Exploring Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function AppNavbar() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Gurps-Utils</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav className="me-auto">
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-	  <div className="App-content">
-	  	<p> Test </p>
-          </div>
-    </div>
+    <>
+        <AppNavbar />
+    </>
   );
 }
 
